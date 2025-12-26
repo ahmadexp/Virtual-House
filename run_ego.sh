a14c07cd5c69172500ef0e3d103623ee57bf2895
Ahmad Byagowi <ahmadexp@gmail.com>
2025-12-25 18:04:09 -0800

    Add files via upload

---
+++ run_ego.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./build/virtualhouse /NOINFO /AUTOEXIT /TRIAL 1
+./build/virtualhouse /NOINFO /AUTOEXIT /TRIAL 2
+./build/virtualhouse /NOINFO /AUTOEXIT /TRIAL 3
+./build/virtualhouse /NOINFO /AUTOEXIT /TRIAL 4
+./build/virtualhouse /NOINFO /AUTOEXIT /TRIAL 5
+./build/virtualhouse /NOINFO /AUTOEXIT /TRIAL 6
+./build/virtualhouse /NOINFO /AUTOEXIT /TRIAL 7
+./build/virtualhouse /NOINFO /AUTOEXIT /TRIAL 8
+
